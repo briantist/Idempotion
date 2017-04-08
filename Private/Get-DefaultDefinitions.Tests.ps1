e326951a469fe03e690da3a16f11ce59ff9b074c
Brian Scholer <brian@briantist.com>
2017-04-08 12:38:11 -0400

    Add Get-DefaultDefinitions

---
+++ Private/Get-DefaultDefinitions.Tests.ps1
@@ -0,0 +1,22 @@
+﻿$modulePath = $PSScriptRoot | Split-Path -Parent
+$moduleName = $modulePath | Split-Path -Leaf
+
+Import-Module -Name $modulePath -Force
+
+InModuleScope $moduleName {
+    Describe "Get-DefaultDefinitions" {
+        $result = Get-DefaultDefinitions
+
+        It 'returns a hashtable' {
+            $result -is [hashtable] | Should Be $true
+        }
+
+        It 'contains a Verbs key that is also a hashtable' {
+            $result.Verbs -is [hashtable] | Should Be $true
+        }
+
+        It 'contains a Snippets key that is also a hashtable' {
+            $result.Snippets -is [hashtable] | Should Be $true
+        }
+    }
+}
---
+++ Private/Get-DefaultDefinitions.ps1
@@ -0,0 +1,8 @@
+﻿function Get-DefaultDefinitions {
+[CmdletBinding()]
+[OutputType([HashTable])]
+param()
+    
+    $definitionPath = $MyInvocation.MyCommand.Module.ModuleBase | Join-Path -ChildPath $MyInvocation.MyCommand.Module.PrivateData.IdempotionConfig.CommandDefinitions
+    Import-PowerShellDataFile -LiteralPath $definitionPath
+}
