e4d1547cfee6eb256bf192cc57e1f17929b8f519
Brian Scholer <brian@briantist.com>
2017-04-08 15:22:31 -0400

    update module import tests and add manifest tests

--- Idempotion.tests.ps1
+++ Idempotion.tests.ps1
@@ -1,8 +1,37 @@
 $modulePath = $PSScriptRoot
 $moduleName = $modulePath | Split-Path -Leaf
 
-Describe 'Idempotion Module' {
-    It 'loads successfully' {
-        { Import-Module -Name $modulePath -Force -ErrorAction Stop } | Should Not Throw
+Describe 'Idempotion Module' -Tags Root {
+    Context "Manifest Destinies" {
+
+        $Script:manifest = $null
+
+        It 'has a valid manifest' {
+            { 
+                $Script:manifest = Test-ModuleManifest -Path "$modulePath\$moduleName.psd1" -ErrorAction Stop 
+            } | Should Not Throw
+        }
+
+        It 'has the right name' {
+            $Script:manifest.Name | Should Be $moduleName
+        }
+
+        It 'has a valid version' {
+            $Script:manifest.Version -as [version] | Should Not BeNullOrEmpty
+        }
+    }
+
+    Context "Loading $moduleName" {
+        $Error.Clear()
+        $module = Import-Module -Name $modulePath -Force -PassThru -ErrorAction SilentlyContinue
+        $errorAfter = $Error.Clone()
+
+        It 'loaded successfully' {
+            $module | Should Not BeNullOrEmpty
+        }
+
+        It 'raised no non-terminating errors' {
+            $errorAfter.Count | Should Be 0
+        }
     }
 }
