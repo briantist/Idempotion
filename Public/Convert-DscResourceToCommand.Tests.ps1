6c9be7ef30a5332419a731da3e9b24bc5e054f31
Brian Scholer <brian@briantist.com>
2017-07-01 15:44:16 -0400

    Add a few tests (not complete)

--- Public/Convert-DscResourceToCommand.Tests.ps1
+++ Public/Convert-DscResourceToCommand.Tests.ps1
@@ -3,8 +3,40 @@
     $Module = (Import-Module -Name ($PSScriptRoot | Split-Path -Parent) -Force -PassThru -ErrorAction Stop)
 )
 
-Describe "Convert-DscResourceToCommand"  {
-    It "does something useful" -Pending {
-        $true | Should Be $false
+InModuleScope $Module.Name {
+    Describe "Convert-DscResourceToCommand (Module parameter set)"  {
+        $resource = Get-DscResource -Name File
+
+        $params = @{
+            Resource = $resource
+        }
+
+        Context 'Defaults' {
+            $result = Convert-DscResourceToCommand @params
+
+            It "returns a module" {
+                $result -is [System.Management.Automation.PSModuleInfo] | Should Be $true
+            }
+
+            It "imports correctly" {
+                { $result | Import-Module -ErrorAction Stop -PassThru | Remove-Module -ErrorAction Ignore } | Should Not Throw
+            }
+
+            It "contains included verbs" {
+                {
+                    $result.ExportedCommands.GetEnumerator().ForEach({
+                        # throw if a verb doesn't match the included verbs
+                        # Verbs: $mod.ExportedCommands.GetEnumerator()|% Value | % ResolvedCommand|% Verb
+
+                    })
+                }
+            }
+        }
+
+        Context 'Not Complete' {
+            It 'is not done yet' {
+                $true | Should Be $false
+            }
+        }
     }
 }
